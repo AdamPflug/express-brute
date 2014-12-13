4d68faa33e5266b5ffc89a16d9004f50d80e5973
marinewater <hartzfear@gmail.com>
2014-12-13 01:55:52 +0100

    res.send(status, body) is deprecated

--- index.js
+++ index.js
@@ -160,11 +160,11 @@ var setRetryAfter = function (res, nextValidRequestDate) {
 };
 ExpressBrute.FailTooManyRequests = function (req, res, next, nextValidRequestDate) {
 	setRetryAfter(res, nextValidRequestDate);
-	res.send(429, {error: {text: "Too many requests in this time frame.", nextValidRequestDate: nextValidRequestDate}});
+	res.status(429).send({error: {text: "Too many requests in this time frame.", nextValidRequestDate: nextValidRequestDate}});
 };
 ExpressBrute.FailForbidden = function (req, res, next, nextValidRequestDate) {
 	setRetryAfter(res, nextValidRequestDate);
-	res.send(403, {error: {text: "Too many requests in this time frame.", nextValidRequestDate: nextValidRequestDate}});
+	res.status(403).send({error: {text: "Too many requests in this time frame.", nextValidRequestDate: nextValidRequestDate}});
 };
 ExpressBrute.FailMark = function (req, res, next, nextValidRequestDate) {
 	res.status(429);
